68433f6a50f962f9f2c69b5a05e548cc5b6c712e
Solovic2 <45718998+Solovic2@users.noreply.github.com>
2019-05-01 00:32:26 +0300

    Update MySpecialLinkedListUtils.java

--- MySpecialLinkedListUtils.java
+++ MySpecialLinkedListUtils.java
@@ -11,132 +11,161 @@ package redgfsdghsd;
  * @author Momen
  */
 public class MySpecialLinkedListUtils {
-    public LinkedListNode head=null;
+	
+public LinkedListNode head=null;
+    
+ public double[]summary (LinkedListNode head){
+     LinkedListNode i =head;
+     LinkedListNode s =head;
+       double sum=0;
+       double average;
+       double meane=0;
+       int max=0;
+       int min=0;
+       double  [] Ar= new double [5];
        int count=0;
-    public  double[]summary (LinkedListNode head){
-        LinkedListNode i =head;
-          int sum=0;
-          double average;
-          int madien;
-          int max=0;
-          int min=0;
-        if(i!=null){
-            max=i.value;
-            min=max;
-        }
-        while(i!=null){
-            if(i.value>max){
-                max=i.value;
-            }
-
-            if(min>i.value){
-                min=i.value;
-            }
-            sum+=i.value;
-            i=i.next;
-            count++;
-        }
-        average=sum/count;
-      double  [] Ar= new double [4];
-      Ar[0]=sum;
-      Ar[1]=max;
-      Ar[2]=min;
-      Ar[3]=average;
-       return Ar;
-    }
-
-    int []a =new int[count];
-
-
-    public  LinkedListNode reverse (LinkedListNode head){
-        LinkedListNode i = head;
-        LinkedListNode prev = null;
-        LinkedListNode  next1= null;
-  
-        while (i != null) { 
-            next1 = i.next; 
-            i.next = prev; 
-            prev = i; 
-            i = next1; 
-        } 
-        head = prev; 
-        return head;
-    }
-    public  boolean palandrom (LinkedListNode head){
-        LinkedListNode i = head;
-         int c=0;
-        while(i!=null){
-            a[c++]=i.value;
-            i=i.next;
-        }
-         int d=0;
-        for(int j=0;j<count/2;j++){
-            if(a[j]==a[count-j-1]){
-                d++;
-            }
-        }
-        if(d==count/2){
-            return true;
-        }else {
-            return false;
-        }
-    }
-
-    public static LinkedListNode evenIndexedElement (LinkedListNode head){
-        LinkedListNode i = head;
-        LinkedListNode j = i.next;
-        while(i!=null && j!=null ){
-        
-        
-            i.next=j.next;
-            i=i.next;
-           
-            if(i!=null)
-                j=i.next;
-        }
-        return head;
-             
-    }
-    public  LinkedListNode removeCentralNode(LinkedListNode head){
-        LinkedListNode i = head;
-        LinkedListNode j=i;
-        if(count%2==0){
-            for(int counter=0;counter<count/2 - 2;counter++){
-                i=i.next;
-                j=i.next;
-            }
-            i.next=j.next;
-            j.next=null;
-        }else if(count %2!=0){
-            for(int counter=0;counter<count/2 - 1;counter++){
-                i=i.next;
-                j=i.next;
-            }
-            i.next=j.next;
-            j.next=null;
-
-         } return head;
-    }
-    public  LinkedListNode insertionSort(LinkedListNode head){
-        LinkedListNode i = head;
-        LinkedListNode j = i.next;
-        LinkedListNode temp;
-        for(int f=0;f<count;f++){
-            while(i!=null && j!=null){
-                if(i.value>j.value){
-                    if
-                    temp=j.next;
-                    j.next=i;
-                    i.next=temp;
-                    j=i.next;
-                }else {
-                    i=i.next;
-                    j=j.next;
-
-                }
-            }
-        }
-        return head;
+       while(s!=null) {
+    	   count++;
+    	   s=s.next;
+       }
+     if(i!=null){
+         max=i.value;
+         min=max;
      }
-   
+     while(i!=null){
+         if(i.value>max){
+             max=i.value;
+         }
+
+         if(min>i.value){
+             min=i.value;
+         }
+         sum+=i.value;
+         i=i.next;
+     }
+     average=sum/count;
+     if((count%2)!=0){
+         i=head;
+         for(int x=0;x<count/2;x++){
+             i=i.next;
+         }
+         meane=i.value;
+     }else if (count%2==0){
+         i=head;
+         LinkedListNode j;
+         for(int x=0;x<(count-1)/2;x++){
+             i=i.next;
+         }
+         j=i.next;
+         meane=(i.value+j.value)/2.0;
+     }
+   Ar[0]=sum;
+   Ar[1]=average;
+   Ar[2]=meane;
+   Ar[3]=max;
+   Ar[4]=min;
+    return Ar;
+ }
+
+
+ public  LinkedListNode reverse (LinkedListNode head){
+     LinkedListNode i = head;
+     LinkedListNode prev = null;
+     LinkedListNode  next1= null;
+
+     while (i != null) { 
+         next1 = i.next; 
+         i.next = prev; 
+         prev = i; 
+         i = next1; 
+     } 
+     head = prev; 
+     return head;
+ }
+ public  boolean palandrom (LinkedListNode head){
+	 LinkedListNode s=head;
+     int count=0;
+     while(s!=null) {
+  	   count++;
+  	   s=s.next;
+     }
+	 int []a =new int[count];
+     LinkedListNode i = head;
+      int c=0;
+     while(i!=null){
+         a[c++]=i.value;
+         i=i.next;
+     }
+      int d=0;
+     for(int j=0;j<count/2;j++){
+         if(a[j]==a[count-j-1]){
+             d++;
+         }
+     }
+     if(d==count/2){
+         return true;
+     }else {
+         return false;
+     }
+ }
+
+public static LinkedListNode evenIndexedElement (LinkedListNode head){
+     LinkedListNode i = head;
+     LinkedListNode j = i.next;
+     while(i!=null && j!=null ){
+     
+     
+         i.next=j.next;
+         i=i.next;
+        
+         if(i!=null)
+             j=i.next;
+     }
+     return head;
+          
+ }
+
+public  LinkedListNode removeCentralNode(LinkedListNode head){
+	LinkedListNode s=head;
+    int count=0;
+    while(s!=null) {
+ 	   count++;
+ 	   s=s.next;
+    }
+     LinkedListNode i = head;
+     LinkedListNode j=i.next;
+     if(count%2==0){
+         for(int counter=0;counter<count/2 - 2;counter++){
+             i=i.next;
+             j=i.next;
+         }
+         i.next=j.next;
+         j.next=null;
+     }else if(count %2!=0){
+         for(int counter=0;counter<count/2 - 1;counter++){
+             i=i.next;
+             j=i.next;
+         }
+         i.next=j.next;
+         j.next=null;
+
+      } return head;
+ }
+ public  LinkedListNode insertionSort(LinkedListNode head){
+	 LinkedListNode i=head,temp=head.next;
+     while(temp != null){
+         i=head;
+         while (i.next != temp){
+             if(i.value>temp.value){
+                 int temp1=temp.value;
+                 temp.value=i.value;
+                 i.value=temp1;
+             }else{
+                 i=i.next;
+             }
+         }
+         temp=temp.next;
+     }
+     return head;
+  }
 }
