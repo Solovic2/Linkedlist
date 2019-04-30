1210ea378d8b7948ddc155a19ea8d0045bd9e728
Solovic2 <45718998+Solovic2@users.noreply.github.com>
2019-05-01 00:33:30 +0300

    Update MySpecialLinkedListUtils.java

--- MySpecialLinkedListUtils.java
+++ MySpecialLinkedListUtils.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 
-package redgfsdghsd;
+package eg.edu.alexu.csd.datastructure.linkedlist;
+
 
 /**
  *
@@ -167,5 +168,36 @@ public  LinkedListNode removeCentralNode(LinkedListNode head){
          temp=temp.next;
      }
      return head;
-  }
+ }
+ //to test solution 
+ public void printL(LinkedListNode shead){
+     LinkedListNode i=shead;
+     while (i != null){
+         System.out.print(i.value);
+         i=i.next;
+     }
+     System.out.println();
+ }
+ public void add (int x,int index){
+     LinkedListNode newnode =new LinkedListNode();
+     newnode.value=x;
+     if (index==0){
+         newnode.next=head;
+         head=newnode;
+     }
+     else{
+         LinkedListNode i=head;
+         for(int j=0;j<index-1;j++){
+             i=i.next;
+         }
+         newnode.next=i.next;
+         i.next=newnode;
+     }
+ }
+
+ public  LinkedListNode gethead(){
+     return head;
+ }
+
 }
+
